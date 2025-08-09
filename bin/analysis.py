5860d71fdd211422abcb5910e52a6f5033f4fa23
gabrielribcesario <gabrielc.ribeiro@hotmail.com>
2025-08-09 00:19:11 -0300

    Fix OOM problem, update visuals

--- bin/analysis.py
+++ bin/analysis.py
@@ -1,3 +1,4 @@
+from matplotlib.ticker import MaxNLocator
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -20,8 +21,7 @@ phi = args.phase
 func = args.function
 tol = args.tolerance
 
-def sine(t):
-    return dc + amp * np.sin(2.0 * np.pi * t / t0 + phi)
+sine = lambda t: dc + amp * np.sin(2.0 * np.pi * t / t0 + phi)
 
 def square(t):
     rad = np.floor(2.0 * t / t0 + phi / np.pi)
@@ -41,14 +41,18 @@ exp = pd.read_csv("data/exp.dat")
 trig = pd.read_csv("data/trig.dat")
 hist = pd.read_csv("data/hist.dat")
 
-n = np.arange(1, hist.shape[0] + 1)
+max_frames = min(exp.shape[0], 51)
+
+n = np.arange(1, exp.shape[0] + 1)
 t = np.linspace(0.0, 3.0 * t0, 300000, endpoint=False) # [s]
-tt, nn = np.meshgrid(t, n[:-1])
+    
+# For creating a dense representation of the approximations
+tt, nn = np.meshgrid(t, n[:max_frames-1]) # [:-1] since it is the number of harmonics and not coefficients (remove DC)
 
 # Exponential approximation as a function of the number of harmonics
-exp_matrix = 2.0 * np.real((exp.Real[1:] + 1.j * exp.Imag[1:]).values.reshape(-1, 1) * np.exp(2.j * np.pi * nn * tt / t0))
+exp_matrix = 2.0 * np.real((exp.Real[1:max_frames] + 1.j * exp.Imag[1:max_frames]).values.reshape(-1, 1) * np.exp(2.j * np.pi * nn * tt / t0))
 exp_matrix = exp_matrix.cumsum(axis=0)
-def exp_appox(k):
+def exp_approx(k):
     if k == 0:
         return np.zeros_like(t)
     elif k== 1: 
@@ -59,9 +63,9 @@ def exp_appox(k):
         raise ValueError
 
 # Trigonometric approximation as a function of the number of harmonics
-trig_matrix = trig.Amplitude[1:].values.reshape(-1, 1) * np.cos(2. * np.pi * nn * tt / t0 + trig.Phase[1:].values.reshape(-1, 1))
+trig_matrix = trig.Amplitude[1:max_frames].values.reshape(-1, 1) * np.cos(2. * np.pi * nn * tt / t0 + trig.Phase[1:max_frames].values.reshape(-1, 1))
 trig_matrix = trig_matrix.cumsum(axis=0)
-def trig_appox(k):
+def trig_approx(k):
     if k == 0:
         return np.zeros_like(t)
     elif k == 1:
@@ -87,11 +91,12 @@ else:
 fig1, ax1 = plt.subplots(2, 1, figsize=(8, 4))
 xmarg = (n.max() - n.min()) * ax1[-1].margins()[0]
 xlim = [n.min() - xmarg, n.max() + xmarg]
-for i, l in enumerate(hist.columns):
+for i, l in enumerate(hist.columns): # MSE and RMSE in different columns
+    ax1[i].xaxis.set_major_locator(MaxNLocator(integer=True))
     ax1[i].plot(n, hist[l], label=l)
     ax1[i].set_ylabel(l)
     ax1[i].set_xlim(xlim)
-    ax1[i].set_xticks(n[::np.where(n.size > 5, 5, 1)])
+    # ax1[i].set_xticks(n[::np.where(n.size > 5, 5, 1)])
 ax1[-1].hlines([tol], xlim[0], xlim[1], lw=1.0, ls="-.", color="k")
 ax1[-1].set_xlabel("# of Coefficients")
 fig1.suptitle("Loss x # of Coefficients")
@@ -102,13 +107,14 @@ fig1.show()
 
 # Plot amplitude of the harmonics
 fig2, ax2 = plt.subplots(figsize=(9, 3))
-tmp = np.where((trig.Phase[1:] > 0.5 * np.pi) 
-               & (trig.Phase[1:] < 1.5 * np.pi), -trig.Amplitude[1:], trig.Amplitude[1:])
-ax2.stem(n[1:] - 1, tmp)
+ax2.xaxis.set_major_locator(MaxNLocator(integer=True))
+ax2.stem(n[1:] - 1, np.where((trig.Phase[1:] > 0.5 * np.pi) & (trig.Phase[1:] < 1.5 * np.pi), 
+                             -trig.Amplitude[1:], 
+                              trig.Amplitude[1:]))
 ax2.set_xlabel("n")
 ax2.set_ylabel("Amplitude")
-# ax2.set_xticks(n[1::np.where(n[1:].size > 5, 5, 1)] - 1)
-fig2.suptitle(f"Amplitude of the n-th {t0**-1:.2f}[Hz] Harmonic")
+if n.size == 2: ax2.set_xticks([1])
+fig2.suptitle(f"Amplitude of the n-th {t0**-1:.2g}[Hz] Harmonic")
 fig2.tight_layout()
 fig2.savefig("figures/amplitude.png")
 fig2.show()
@@ -116,7 +122,8 @@ fig2.show()
 # Plot approximation and squared error
 fig3, axes3 = plt.subplots(2, 1, figsize=(10, 5))
 ax3, ax4 = axes3
-title3 = fig3.suptitle("Original Signal x Fourier Series\n# of Coefficients: \nMSE = \nRMSE = ")
+title_str = "Original Signal x Fourier Series\n# of Harmonics: {:0{}}\nMSE = {:.7f}\nRMSE = {:.7f}"
+title3 = fig3.suptitle(title_str)
 # Approximation
 ax3.plot(t, signal, label="Original signal")
 fs_line = ax3.plot(t, np.zeros_like(t), label="Fourier Series Approximation")[0]
@@ -124,27 +131,28 @@ ax3.set_ylabel("Amplitude")
 ax3.set_ylim([dc - amp * 1.3, dc + amp * 1.3])
 ax3.yaxis.set_inverted(False)
 # Squared Error
-se = (signal - trig_appox(1))**2
-mse_line = ax4.plot(t, se, label="Squared Error", c="C2")[0]
+se = (signal - trig_approx(1))**2
+mse_line = ax4.plot(t, se, label="Squared error", c="C2")[0]
 ax4.set_ylim([-ax4.margins()[1], se.max() + ax4.margins()[1]])
 ax4.set_xlabel("Time [s]")
 ax4.set_ylabel("Squared Error")
 
-zpad = int(np.log10(n.max())) + 1 # Zero-pad string
+zpad = min(int(np.log10(n.max())) + 1, 2) # Zero-pad string
 def update_frame(frame):
-    y = trig_appox(frame + 1)
+    y = trig_approx(frame + 1)
     se = (signal - y)**2
     mse, rmse = hist.MSE[frame], hist.RMSE[frame]
 
     fs_line.set_ydata(y)
     mse_line.set_ydata(se)
-    title3.set_text(f"Original Signal x Fourier Series\n# of Coefficients: {str(frame + 1).zfill(zpad)}\nMSE = {mse:.7f}\nRMSE = {rmse:.7f}")
+    title3.set_text(title_str.format(frame, zpad, mse, rmse))
     return (fs_line, mse_line, title3)
 
 # Only animate the 50 first harmonics.
+fps = 5
 fig3.legend(loc="upper right")
 fig3.tight_layout()
-ani3 = animation.FuncAnimation(fig=fig3, func=update_frame, frames=min(hist.shape[0], 50), interval=250, repeat_delay=750)
-ani3.save("figures/approximation.gif")
+ani3 = animation.FuncAnimation(fig=fig3, func=update_frame, frames=max_frames, interval=1000 / fps, repeat_delay=250)
+ani3.save("figures/approximation.gif", writer=animation.writers["pillow"](fps=fps))
 fig3.savefig("figures/func.png")
 plt.show()
